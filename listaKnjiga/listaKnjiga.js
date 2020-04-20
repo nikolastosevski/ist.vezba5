a800fafd1630d8001d321a384e685ec1954e4041
Nikola <nikolastosevski@hotmail.com>
2020-04-20 12:34:46 +0200

    Pravljenje liste knjiga..

---
+++ listaKnjiga/listaKnjiga.js
@@ -0,0 +1,6 @@
+var lista = [{id:1, naslov:"BEZIMENI GRAD", autor:"H. F. Lavkraft"},
+             {id:2, naslov:"NEBESKI BEDEMI", autor:"Saša Kuzmanović"},
+             {id:3, naslov:"GRIMIZNI POHOD", autor:"Brajan Maklelan"},
+             {id:4, naslov:"OKEAN OD PAPIRA", autor:"Zoran Penevski"},
+             {id:5, naslov:"KOMPLET-PROLAZ", autor:"Džastin Kronin"}];
+exports.listaKnjiga = lista;
